0a206f18525b38890fb6279d6961bba512dcfaf7
zuanki <nguyenxuanhoakhtn@gmail.com>
2023-12-14 16:32:18 +0700

    Enhance UI for company leadership

---
+++ app/(admin)/company_leader/components/Staff.tsx
@@ -0,0 +1,182 @@
+'use client';
+import * as React from 'react';
+
+import Box from '@mui/joy/Box';
+
+
+import StaffTable from './StaffTable';
+import Button from '@mui/joy/Button';
+import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/joy/Typography';
+import Modal from '@mui/joy/Modal';
+
+import FormControl from '@mui/joy/FormControl';
+import Input from '@mui/joy/Input';
+import Card from '@mui/joy/Card';
+import CardActions from '@mui/joy/CardActions';
+import CardOverflow from '@mui/joy/CardOverflow';
+import Stack from '@mui/joy/Stack';
+import FormLabel from '@mui/joy/FormLabel';
+import Divider from '@mui/joy/Divider';
+
+import CountrySelector from './CountrySelector';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+
+
+
+
+
+export default function Staff() {
+    const [openAddStaff, setOpenAddStaff] = React.useState(false);
+
+    const renderAddStaffModal = () => {
+        return (
+            <Modal
+                open={openAddStaff}
+                onClose={() => setOpenAddStaff(false)}
+                // title="Add Staff"
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Card
+                    style={{
+                        width: '100%',
+                        maxWidth: 600,
+                        // borderRadius: 12,
+                        // overflow: 'hidden',
+                    }}
+                >
+                    <Box sx={{ mb: 1 }}>
+                        <Typography level="title-md">Personal Information</Typography>
+                        <Typography level="body-sm">
+                            Customize how your profile information will apper to the networks.
+                        </Typography>
+                    </Box>
+                    <Divider />
+                    <Stack
+                        direction="row"
+                    >
+                        <Stack direction="column" spacing={1}>
+
+                        </Stack>
+                        <Stack spacing={2} sx={{ flexGrow: 1 }}>
+                            <Stack spacing={1}>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl
+                                >
+                                    <Input size="sm" placeholder="Your name" />
+                                </FormControl>
+                                <FormLabel>Phone</FormLabel>
+                                <FormControl
+                                >
+                                    <Input size="sm" placeholder="Phone" />
+                                </FormControl>
+
+                            </Stack>
+                            <Stack direction="row" spacing={2}>
+                                <FormControl>
+                                    <FormLabel>Role</FormLabel>
+                                    <Select
+                                        size="sm"
+                                        defaultValue="1"
+                                    >
+                                        <Option value="1">
+                                            <Typography level='body-sm'>
+                                                GATHERING
+                                            </Typography>
+                                        </Option>
+                                        <Option value="2">
+                                            <Typography level='body-sm'>
+                                                TRANSACTION
+                                            </Typography>
+                                        </Option>
+                                    </Select>
+                                </FormControl>
+                                <FormControl sx={{ flexGrow: 1 }}>
+                                    <FormLabel>Email</FormLabel>
+                                    <Input
+                                        size="sm"
+                                        type="email"
+                                        startDecorator={<EmailRoundedIcon />}
+                                        placeholder="@magic-post.com"
+                                        sx={{ flexGrow: 1 }}
+                                    />
+                                </FormControl>
+                            </Stack>
+                            <div>
+                                <CountrySelector />
+                            </div>
+                        </Stack>
+                    </Stack>
+                    <CardOverflow >
+                        <CardActions sx={{ alignSelf: 'flex-end', pt: 2 }}>
+                            <Button size="sm" variant="outlined" color="neutral" onClick={() => setOpenAddStaff(false)}>
+                                Cancel
+                            </Button>
+                            <Button size="sm" variant="outlined" color="primary">
+                                Save
+                            </Button>
+                        </CardActions>
+                    </CardOverflow>
+                </Card>
+
+            </Modal>
+        );
+    }
+
+    return (
+        <div>
+            <Box
+                component="main"
+                className="MainContent"
+                sx={{
+                    px: { xs: 2, md: 6 },
+                    pt: {
+                        xs: 'calc(12px + var(--Header-height))',
+                        sm: 'calc(12px + var(--Header-height))',
+                        md: 3,
+                    },
+                    pb: { xs: 2, sm: 2, md: 3 },
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    minWidth: 0,
+                    height: '100dvh',
+                    gap: 1,
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        mb: 1,
+                        gap: 1,
+                        flexDirection: { xs: 'column', sm: 'row' },
+                        alignItems: { xs: 'start', sm: 'center' },
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <Typography level="h2" component="h1">
+                        Staffs
+                    </Typography>
+                    <Button
+                        color="primary"
+                        startDecorator={<AddIcon />}
+                        size="sm"
+                        variant="outlined"
+                        onClick={() => setOpenAddStaff(true)}
+                    >
+                        Add Staff
+                    </Button>
+                </Box>
+                <StaffTable />
+            </Box>
+            {renderAddStaffModal()}
+        </div>
+    );
+}
---
+++ app/(admin)/company_leader/components/index.tsx
@@ -0,0 +1,33 @@
+'use client';
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+
+import palette from '../../../styles/palette';
+import Sidebar from './SideBar';
+import Header from './Header';
+import Staff from './Staff';
+import Office from './Office';
+
+
+
+
+export default function CompanyLeader() {
+
+    return (
+        <CssVarsProvider disableTransitionOnChange theme={palette}>
+            <CssBaseline />
+            <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+                <Header />
+                <Sidebar />
+                {/* <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default' }}>
+                    <Staff />
+                    <Office />
+
+                </Box> */}
+                <Office />
+            </Box>
+        </CssVarsProvider>
+    );
+}
