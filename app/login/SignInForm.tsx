223e12be0916408ca68fd2df1d5744fc94f10249
PhamMinhVuong <100543300+PhamMinhVuong21020425@users.noreply.github.com>
2023-12-16 13:17:30 +0700

    Login, sign up with google, session, logout

---
+++ app/login/SignInForm.tsx
@@ -0,0 +1,240 @@
+'use client';
+import * as React from 'react';
+import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
+import GlobalStyles from '@mui/joy/GlobalStyles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Checkbox from '@mui/joy/Checkbox';
+import Divider from '@mui/joy/Divider';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel, { formLabelClasses } from '@mui/joy/FormLabel';
+import IconButton, { IconButtonProps } from '@mui/joy/IconButton';
+import Link from '@mui/joy/Link';
+import Input from '@mui/joy/Input';
+import Typography from '@mui/joy/Typography';
+import Stack from '@mui/joy/Stack';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+// import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import GoogleIcon from './GoogleIcon';
+import styled from '@emotion/styled';
+import { redirect } from 'next/navigation'
+
+import palette from '../styles/palette';
+
+interface FormElements extends HTMLFormControlsCollection {
+    email: HTMLInputElement;
+    password: HTMLInputElement;
+    persistent: HTMLInputElement;
+}
+interface SignInFormElement extends HTMLFormElement {
+    readonly elements: FormElements;
+}
+
+function ColorSchemeToggle(props: IconButtonProps) {
+    const { onClick, ...other } = props;
+    const { mode, setMode } = useColorScheme();
+    const [mounted, setMounted] = React.useState(false);
+    React.useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) {
+        return <IconButton size="sm" variant="outlined" color="neutral" disabled />;
+    }
+    return (
+        <IconButton
+            id="toggle-mode"
+            size="sm"
+            variant="outlined"
+            color="neutral"
+            aria-label="toggle light/dark mode"
+            {...other}
+            onClick={(event) => {
+                if (mode === 'light') {
+                    setMode('dark');
+                } else {
+                    setMode('light');
+                }
+                onClick?.(event);
+            }}
+        >
+            {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+        </IconButton>
+    );
+}
+
+
+export default function SignInForm() {
+    return (
+        <CssVarsProvider defaultMode="light" disableTransitionOnChange theme={palette}>
+            <CssBaseline />
+            <GlobalStyles
+                styles={{
+                    ':root': {
+                        '--Collapsed-breakpoint': '769px', // form will stretch when viewport is below `769px`
+                        '--Cover-width': '50vw', // must be `vw` only
+                        '--Form-maxWidth': '800px',
+                        '--Transition-duration': '0.4s', // set to `none` to disable transition
+                        'display': 'flex',
+                        'justify-content': 'center',
+                    },
+                }}
+            />
+            <Box
+                sx={(theme) => ({
+                    width:
+                        'clamp(100vw - var(--Cover-width), (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)',
+                    transition: 'width var(--Transition-duration)',
+                    transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+                    position: 'relative',
+                    zIndex: 1,
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    backdropFilter: 'blur(12px)',
+                    backgroundColor: 'rgba(255 255 255 / 0.2)',
+                    [theme.getColorSchemeSelector('dark')]: {
+                        backgroundColor: 'rgba(19 19 24 / 0.4)',
+                    },
+                })}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        minHeight: '100dvh',
+                        width:
+                            'clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)',
+                        maxWidth: '100%',
+                        px: 2,
+                    }}
+                >
+                    <Box
+                        component="header"
+                        sx={{
+                            py: 3,
+                            display: 'flex',
+                            alignItems: 'left',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <Box sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
+                            <IconButton variant="soft" color="primary" size="sm">
+                                <LocalShippingIcon />
+                            </IconButton>
+                            <Typography level="title-lg">MagicPost</Typography>
+                        </Box>
+                        <ColorSchemeToggle />
+                    </Box>
+                    <Box
+                        component="main"
+                        sx={{
+                            my: 'auto',
+                            py: 2,
+                            pb: 5,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: 2,
+                            width: 400,
+                            maxWidth: '100%',
+                            mx: 'auto',
+                            borderRadius: 'sm',
+                            '& form': {
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: 2,
+                            },
+                            [`& .${formLabelClasses.asterisk}`]: {
+                                visibility: 'hidden',
+                            },
+                        }}
+                    >
+                        <Stack gap={4} sx={{ mb: 2 }}>
+                            <Stack gap={1}>
+                                <Typography level="h3">Sign in</Typography>
+                                <Typography level="body-sm">
+                                    New to company?{' '}
+                                    <Link href='/signup' level="title-sm">
+                                        Sign up!
+                                    </Link>
+                                </Typography>
+                            </Stack>
+                            <form method='POST' action='/auth/login-google'>
+                                <Button
+                                    type='submit'
+                                    variant="soft"
+                                    color="neutral"
+                                    fullWidth
+                                    startDecorator={<GoogleIcon />}
+                                >
+                                    Continue with Google  
+                                </Button>
+                            </form>
+                        </Stack>
+                        <Divider
+                            sx={(theme) => ({
+                                [theme.getColorSchemeSelector('light')]: {
+                                    color: { xs: '#FFF', md: 'text.tertiary' },
+                                    '--Divider-lineColor': {
+                                        xs: '#FFF',
+                                        md: 'var(--joy-palette-divider)',
+                                    },
+                                },
+                            })}
+                        >
+                            or
+                        </Divider>
+                        <Stack gap={4} sx={{ mt: 2 }}>
+                            <form
+                                // onSubmit={(event: React.FormEvent<SignInFormElement>) => {
+                                //     event.preventDefault();
+                                //     const formElements = event.currentTarget.elements;
+                                //     const data = {
+                                //         email: formElements.email.value,
+                                //         password: formElements.password.value,
+                                //         persistent: formElements.persistent.checked,
+                                //     };
+                                //     alert(JSON.stringify(data, null, 2));
+                                // }}
+                                method='POST'
+                                action='/auth/login-password'
+                            >
+                                <FormControl required>
+                                    <FormLabel>Email</FormLabel>
+                                    <Input type="email" name="email" placeholder="example@gmail.com" />
+                                </FormControl>
+                                <FormControl required>
+                                    <FormLabel>Password</FormLabel>
+                                    <Input type="password" name="password" placeholder="********" />
+                                </FormControl>
+                                <Stack gap={4} sx={{ mt: 2 }}>
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'space-between',
+                                            alignItems: 'center',
+                                        }}
+                                    >
+                                        <Checkbox size="sm" label="Remember me" name="persistent" />
+                                        <Link level="title-sm" href="#replace-with-a-link">
+                                            Forgot your password?
+                                        </Link>
+                                    </Box>
+                                    <Button type='submit' fullWidth variant='outlined' color='primary'>
+                                        Sign in
+                                    </Button>
+                                </Stack>
+                            </form>
+                        </Stack>
+                    </Box>
+                    <Box component="footer" sx={{ py: 3 }}>
+                        <Typography level="body-xs" textAlign="center">
+                            © MagicPost {new Date().getFullYear()}
+                        </Typography>
+                    </Box>
+                </Box>
+            </Box>
+        </CssVarsProvider>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,16 @@ import Banner from './components/Banner/Banner';
 import Services from './components/Services/Services';
 import Navbarin from './components/Navbar';
 import Footer from './components/Footer/Footer'
+// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+// import { cookies } from 'next/headers';
 
-export default function IndexPage() {
+export default async function IndexPage() {
+  // const supabase = createServerComponentClient( {cookies} )
+  // const { data } = await supabase.from("auth.users").select();
+
   return (
     <div>
+      {/* <pre>{ JSON.stringify(data, null, 2) }</pre> */}
       <Navbarin />
       <Banner />
       <Services />
